777f4406bb3b5a1fe92a59c9fe0b25a068c973bf
ldxiaoxie <ldxiaoxie@163.com>
2018-12-16 19:56:16 +0800

    lhhhh

---
+++ day01git/src/com/itheima/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+    }
+}
