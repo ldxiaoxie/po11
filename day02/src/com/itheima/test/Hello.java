48dfa9a8e55644dd92a99b4e655989cd4fbb7e9f
ldxiaoxie <ldxiaoxie@163.com>
2018-12-16 20:12:32 +0800

    123

---
+++ day02/src/com/itheima/test/Hello.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+    }
+}
